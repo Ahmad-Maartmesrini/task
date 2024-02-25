fcb1d4d72757cc8e32b4edd767ef824dde43b8cc
Ahmad Maartmesrini <Ahmad-Maartmesrini@outlook.com>
2024-02-25 19:48:55 +0300

    add components & style

---
+++ src/components/balance/Balance.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import "./balance.css";
+
+export default function Balance() {
+  const [balance, setBalance] = useState("loading...");
+
+  useEffect(() => {
+    console.log("I just ran");
+    const url: string = `https://api.etherscan.io/api?module=account&action=balance&address=0xdac17f958d2ee523a2206206994597c13d831ec7&tag=latest&apikey=U1EY3SWGNYUZ26W45E9FUCJW4CF3GPEWR6`;
+    fetch(url)
+      .then((response: Response) => response.json())
+      .then((data) => {
+        const balance = data.result / 1e6; // Convert wei to USDT
+        setBalance(balance.toString());
+      })
+      .catch((error) => {
+        console.error(error);
+        setBalance("Loading failed.");
+      });
+  }, []);
+
+  return (
+    <div className="balance">
+      <h1>The USDT balance of the Provided address is:</h1>
+      <h1>{balance}</h1>
+    </div>
+  );
+}
---
+++ src/components/lastBlock/LastBlock.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Network, Alchemy } from "alchemy-sdk";
+import "./lastBlock.css";
+
+const LastBlockNumber = () => {
+  const network: Network = Network.ETH_MAINNET;
+  const apiKey: string = "nnrZdk6AtGsHrw2NFLaF4fRdUMuEiuDU"; // my API Key from Alchemy
+  const alchemy = new Alchemy({ network, apiKey });
+  const [number, setNumber] = useState("loading...");
+
+  async function fetchBlockNumber() {
+    try {
+      const blockNumber: number = await alchemy.core.getBlockNumber();
+      setNumber(blockNumber.toString());
+    } catch (error) {
+      console.error("Error fetching block number:", error);
+      setNumber("Loading failed, Click Refresh Please.");
+    }
+  }
+  fetchBlockNumber();
+
+  return (
+    <div className="block-number">
+      <h1>The Last Block number of Ethereum mainnet is:</h1>
+      <h1>{number}</h1>
+      <button onClick={fetchBlockNumber} className="refresh">
+        Refresh
+      </button>
+    </div>
+  );
+};
+
+export default LastBlockNumber;
