9b316e3942e85b01569bf8ce533bfe042040d712
Ahmad Maartmesrini <Ahmad-Maartmesrini@outlook.com>
2024-02-28 17:16:48 +0300

    Update LastBlock.test.tsx

--- src/components/__tests__/LastBlock.test.tsx
+++ src/components/__tests__/LastBlock.test.tsx
@@ -1,12 +1,7 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import LastBlock from "../lastBlock/LastBlock";
 
 describe("LastBlock component", () => {
-  beforeEach(() => {
-    jest.resetModules();
-  });
-
   test("displays the loading state initially", async () => {
     render(<LastBlock />);
     expect(screen.getByText(/loading.../i)).toBeInTheDocument();
